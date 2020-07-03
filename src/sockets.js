c9a5ed3142d0d1b17443582363cf22eafe110239
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-07-03 17:25:15 -0500

    News features added like 'new user', 'send message' like object, 'disconnect' and 'updateNicknames'

--- src/sockets.js
+++ src/sockets.js
@@ -3,13 +3,44 @@
 //SOCKET LISTEN NEW CONEXION OF CLIENT
 //SOCKET SERVER
 module.exports = function(io) {
+    let nicknames = [
+        'Victoriasaurio',
+        'David',
+        'Sara'
+    ];
+
     io.on('connection', socket => {
         console.log('new client conected');
 
-        socket.on('send message', function(data) { //DATA OF ONE CLIENT
-            io.sockets.emit('new message', data); //SERVER TRANSMITS THE MESSAGE TO ALL CLIENTSS
-        })
-    })
+        socket.on('new user', (data, cb) => {
+            if (nicknames.indexOf(data) != -1) { //.indexOf RETURN AN INDEX LIKE [0,1,2,...] || -1 MEANS: NEW USER  
+                cb(false);
+            } else {
+                cb(true);
+                socket.nickname = data; //SAVE (data)NAME USER IN SOCKET
+                nicknames.push(socket.nickname); //SAVE IN nicknames ARRAY A NEW USER
+                updateNicknames(); //SEND ALL USERS OF 'nicknames' ARRAY
+            }
+        });
+
+        socket.on('send message', data => { //DATA OF ONE CLIENT
+            io.sockets.emit('new message', {
+                msg: data,
+                nick: socket.nickname
+            }); //SERVER TRANSMITS THE MESSAGE AND USER FROM AN OBJECT TO ALL CLIENTSS
+        });
+
+        socket.on('disconnect', data => {
+            if (!socket.nickname) return; //RETURNS EMPTY BECAUSE THERE IS NO USERNAME CONNECTED TO THE SOCKET OBJECT
+            nicknames.splice(nicknames.indexOf(socket.nickname), 1); //DELETE ONE USER, IF IT HAS BEEN DISCONNECTED
+            updateNicknames(); //UPDATE LIST OF CONNECTED USERS
+        });
+
+        //CONTAINS LIST OD CONNECTED USERS
+        function updateNicknames() {
+            io.sockets.emit('usernames', nicknames);
+        }
+    });
 }
 
 //SOCKET STORE ONE CLIENT
