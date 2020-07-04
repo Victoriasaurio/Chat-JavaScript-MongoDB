721f1e5c215114be1e32fd1af6d9c989584064ba
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-07-03 22:33:14 -0500

    User connected changed from array to object, listen private event and error message

--- src/sockets.js
+++ src/sockets.js
@@ -3,42 +3,60 @@
 //SOCKET LISTEN NEW CONEXION OF CLIENT
 //SOCKET SERVER
 module.exports = function(io) {
-    let nicknames = [
-        'Victoriasaurio',
-        'David',
-        'Sara'
-    ];
+    let users = {};
 
     io.on('connection', socket => {
         console.log('new client conected');
 
         socket.on('new user', (data, cb) => {
-            if (nicknames.indexOf(data) != -1) { //.indexOf RETURN AN INDEX LIKE [0,1,2,...] || -1 MEANS: NEW USER  
+            if (data in users) { //(nicknames.indexOf(data) != -1)  .indexOf RETURN AN INDEX LIKE [0,1,2,...] || -1 MEANS: NEW USER  
                 cb(false);
             } else {
                 cb(true);
                 socket.nickname = data; //SAVE (data)NAME USER IN SOCKET
-                nicknames.push(socket.nickname); //SAVE IN nicknames ARRAY A NEW USER
+                users[socket.nickname] = socket; //EVERY USER CONNECTED HAS INFO OF SOCKET || //users.push(socket.nickname); //SAVE IN nicknames ARRAY A NEW USER
                 updateNicknames(); //SEND ALL USERS OF 'nicknames' ARRAY
             }
         });
 
-        socket.on('send message', data => { //DATA OF ONE CLIENT
-            io.sockets.emit('new message', {
-                msg: data,
-                nick: socket.nickname
-            }); //SERVER TRANSMITS THE MESSAGE AND USER FROM AN OBJECT TO ALL CLIENTSS
+        socket.on('send message', (data, cb) => { //DATA OF ONE CLIENT
+            // /w vic mensaje....
+            var msg = data.trim(); //REMOVE EXTRA SPACES FROM TEXTS
+
+            if (msg.substr(0, 3) === '/w ') { //GO THROUGH THE INDEXES
+                msg = msg.substr(3); //NEW MESSAGE FROM POSITION 3(SPACE)
+                const index = msg.indexOf(' '); //ANALYZING THE SPACE
+                if (index !== -1) {
+                    var name = msg.substring(0, index); //GET USERNAME 
+                    var msg = msg.substring(index + 1); //NEW MESSAGE FROM POSITION 1 AFTER INDEX
+                    if (name in users) {
+                        users[name].emit('whisper', { //SENDING MESSAGE LIKE OBJECT FROM EVENT 'whisper'
+                            msg,
+                            nick: socket.nickname //ISSUING USER
+                        });
+                    } else {
+                        cb('Error! Please enter a valid user');
+                    }
+                } else {
+                    cb('Error! Please enter your message');
+                }
+            } else {
+                io.sockets.emit('new message', {
+                    msg: data,
+                    nick: socket.nickname
+                }); //SERVER TRANSMITS THE MESSAGE AND USER FROM AN OBJECT TO ALL CLIENTSS
+            }
         });
 
         socket.on('disconnect', data => {
             if (!socket.nickname) return; //RETURNS EMPTY BECAUSE THERE IS NO USERNAME CONNECTED TO THE SOCKET OBJECT
-            nicknames.splice(nicknames.indexOf(socket.nickname), 1); //DELETE ONE USER, IF IT HAS BEEN DISCONNECTED
+            delete users[socket.nickname]; //REMOVE USER WITH THE KEY 'socket.nickname' || //nicknames.splice(nicknames.indexOf(socket.nickname), 1); //DELETE ONE USER, IF IT HAS BEEN DISCONNECTED
             updateNicknames(); //UPDATE LIST OF CONNECTED USERS
         });
 
         //CONTAINS LIST OD CONNECTED USERS 
         function updateNicknames() {
-            io.sockets.emit('usernames', nicknames);
+            io.sockets.emit('usernames', Object.keys(users)); //GET KEYS OF OBJECT 'users'
         }
     });
 }
