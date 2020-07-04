d92828b001ad5fdbfb89b77823e2e9303ee9082c
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-07-03 23:56:45 -0500

    Created Schema of database

---
+++ src/models/chat.js
@@ -0,0 +1,14 @@
+//SCHEMA OF MESSAGE
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const chatSchema = new Schema({
+    nick: String,
+    msg: String,
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
